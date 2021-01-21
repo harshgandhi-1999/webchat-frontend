77948af2c8019bc91b04d66b699c715b4c3bd156
harsh gandhi <harshgandhi043@gmail.com>
2021-01-21 12:11:17 +0530

    sidebar header style done

--- src/components/DropdownMenu/DropdownMenu.js
+++ src/components/DropdownMenu/DropdownMenu.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import {DropdownButton,Dropdown} from 'react-bootstrap'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faEllipsisV} from "@fortawesome/free-solid-svg-icons";
 
 const DropdownMenu = () => {
     return (
-            <ul className="dropdown-menu bg-gray-100 absolute py-2 text-gray-700 rounded right-0 top-full z-10">
-                <li className="py-2 px-4 hover:bg-gray-200 block cursor-pointer whitespace-nowrap">Add Contact</li>
-                <li className="py-2 px-4 hover:bg-gray-200 block cursor-pointer whitespace-nowrap">Add Contact</li>
-                <li className="py-2 px-4 hover:bg-gray-200 block cursor-pointer whitespace-nowrap">Add Contact</li>
-            </ul>
+        <DropdownButton>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </DropdownButton>
     )
 }
 
--- src/components/SidebarComponent/SidebarHeader.js
+++ src/components/SidebarComponent/SidebarHeader.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState,useEffect } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUserCircle,
@@ -11,27 +11,6 @@ import Backdrop from "../Backdrop/Backdrop";
 
 const SidebarHeader = () => {
 
-  const [showMenu,setShowMenu] = useState(false);
-  const dropdownContainerRef = useRef();
-
-  const toggleMenu = ()=>{
-    setShowMenu((prevshow)=>!prevshow);
-  }
-
-  const handleClickOutside = event => {
-    if (dropdownContainerRef.current && !dropdownContainerRef.current.contains(event.target)) {
-      setShowMenu(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown',handleClickOutside);
-
-    return ()=>{
-      document.removeEventListener("mouseup",handleClickOutside);
-    }
-  }, [])
-
   return (
     <React.Fragment>
       <div className="sidebar-header d-flex flex-row justify-content-between mb-2">
@@ -41,30 +20,18 @@ const SidebarHeader = () => {
           <FontAwesomeIcon
             icon={faUserCircle}
             color="white"
-            style={{ fontSize: "2rem" }}
+            style={{fontSize: "2rem" }}
           />
         </div>
         <div className="user-actions d-flex flex-row align-items-center">
           <div className="new-chat-btn mr-4">
-            <button className="focus:outline-none">
-              <FontAwesomeIcon
+            <FontAwesomeIcon
                 icon={faPlusCircle}
                 color="white"
-                style={{ fontSize: "1.5rem" }}
-              />
-            </button>
+                style={{ fontSize: "1.5rem",cursor:'pointer' }}
+            />
           </div>
-          <div className="dropdown inline-block relative" ref={dropdownContainerRef}>
-            <button className="focus:outline-none">
-              <FontAwesomeIcon
-                icon={faEllipsisV}
-                color="white"
-                style={{ fontSize: "1.5rem" }}
-                onClick={toggleMenu}
-              />
-            </button>
-            {showMenu && <DropdownMenu/>}
-          </div>
+            <DropdownMenu/>
         </div>
       </div>
     </React.Fragment>
