e2be44f792687b72cbba6b0afbd232c7171f62e1
harsh gandhi <harshgandhi043@gmail.com>
2021-09-05 13:35:26 +0530

    implemented e2e encryption

---
+++ src/e2e/aes.js
@@ -0,0 +1,19 @@
+const CryptoJS = require("crypto-js");
+
+const secretKey = process.env.REACT_APP_PRIVATE_KEY;
+
+function encryptData(data) {
+  data = JSON.stringify(data);
+  const cipherText = CryptoJS.AES.encrypt(data, secretKey).toString();
+
+  return cipherText;
+}
+
+function decryptData(cipherText) {
+  const bytes = CryptoJS.AES.decrypt(cipherText, secretKey);
+  const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
+  return decryptedData;
+}
+
+module.exports = { encryptData, decryptData };
